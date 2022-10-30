23eecb169c7224a2bb0bd6436f06515fd7efd589
Bedri FIDNIK <116302631+BedriF@users.noreply.github.com>
2022-10-30 14:10:26 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        double boy,kilo;
+        double vke;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Boyunuzu giriniz (metre cinsizden) : ");
+        boy = input.nextDouble();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Kilonuzu giriniz (kg cinsinden) : ");
+        kilo = input2.nextDouble();
+
+        vke=kilo/(boy*boy);
+        System.out.println("Vücut kitle indeksiniz :" + vke);
+
+
+    }
+}
